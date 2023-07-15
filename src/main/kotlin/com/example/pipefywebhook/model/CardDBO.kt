ef66eb7360b0f9c54636d780df544852aa3d51c8
pedro <pedroajs93@gmail.com>
2023-07-15 15:03:51 -0300

    cardDBO toModel extension function

--- src/main/kotlin/com/example/pipefywebhook/model/CardDBO.kt
+++ src/main/kotlin/com/example/pipefywebhook/model/CardDBO.kt
@@ -8,8 +8,18 @@ import javax.persistence.Id
 @Entity
 class CardDBO(
     @Id
-    val id: UUID,
+    @GeneratedValue
+    val id: UUID?,
     val name: String,
     val email: String,
     val occupation: String,
-)
+){
+    fun toModel() = id?.let {
+        Card(
+        id = it,
+        name = name,
+        email = email,
+        occupation = occupation,
+    )
+    }
+}
