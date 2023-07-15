1f7c6abbcb34cac7a3b9d80c4cdc9eae81256131
pedro <pedroajs93@gmail.com>
2023-07-15 13:01:17 -0300

    test log persistence

--- src/test/kotlin/com/example/pipefywebhook/PipefywebhookApplicationTests.kt
+++ src/test/kotlin/com/example/pipefywebhook/PipefywebhookApplicationTests.kt
@@ -1,13 +1,28 @@
 package com.example.pipefywebhook
 
+import com.example.pipefywebhook.model.CardDBO
+import com.example.pipefywebhook.repository.LogRepository
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.UUID
 
 @SpringBootTest
-class PipefywebhookApplicationTests {
+class PipefywebhookApplicationTests{
 
+	@Autowired
+	lateinit var logRepository: LogRepository
 	@Test
 	fun contextLoads() {
 	}
 
+
+	@Test
+	fun `should save a log when requested`(){
+		val id = UUID.randomUUID()
+		val cardDBO = CardDBO(id,"Pedro", "pedroajs@gmail.com", "Desenvolvedor")
+		val cardLogSaved = logRepository.save(cardDBO)
+		Assertions.assertEquals(cardDBO.id, cardLogSaved.id)
+	}
 }
