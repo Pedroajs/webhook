f0358423a64afb67079f1310ee5a4d486462d339
pedro <pedroajs93@gmail.com>
2023-07-15 15:05:25 -0300

    using service layer

--- src/test/kotlin/com/example/pipefywebhook/PipefywebhookApplicationTests.kt
+++ src/test/kotlin/com/example/pipefywebhook/PipefywebhookApplicationTests.kt
@@ -1,7 +1,9 @@
 package com.example.pipefywebhook
 
 import com.example.pipefywebhook.model.CardDBO
+import com.example.pipefywebhook.model.CardDTO
 import com.example.pipefywebhook.repository.LogRepository
+import com.example.pipefywebhook.service.LogService
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,19 +12,20 @@ import java.util.UUID
 
 @SpringBootTest
 class PipefywebhookApplicationTests{
-
 	@Autowired
-	lateinit var logRepository: LogRepository
+	lateinit var logService: LogService
 	@Test
 	fun contextLoads() {
 	}
 
-
 	@Test
 	fun `should save a log when requested`(){
-		val id = UUID.randomUUID()
-		val cardDBO = CardDBO(id,"Pedro", "pedroajs@gmail.com", "Desenvolvedor")
-		val cardLogSaved = logRepository.save(cardDBO)
-		Assertions.assertEquals(cardDBO.id, cardLogSaved.id)
+		val cardDTO = CardDTO(id = null,
+			name = "Pedro",
+			email ="pedroajs@gmail.com",
+			occupation = "Desenvolvedor")
+			.toDBO()
+		val cardLogSaved = logService.saveLog(cardDTO)
+		Assertions.assertEquals(cardDTO.id, cardLogSaved.id)
 	}
 }
